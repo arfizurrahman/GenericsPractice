2f5b991fba73ed0ab5326708d6628ffb21c24d06
arfizurrahman <arfizrahman0@gmail.com>
2019-01-12 14:49:01 +0600

    Generic type test1

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Generics
 {
+    public class GenericList<T>
+    {
+        public void Add(T input)
+        {
+        }
+    }
+
     class Program
     {
+        private class MyClass1
+        {
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declare a list of type int.
+            GenericList<int> intList = new GenericList<int>();
+            intList.Add(1);
+
+            //Declare list of type string 
+            GenericList<string> stringList = new GenericList<string>();
+            stringList.Add("item1");
+
+            //Declare list of type MyClass1
+            GenericList<MyClass1> myClassList = new GenericList<MyClass1>();
+            myClassList.Add(new MyClass1());
+            Console.ReadKey();
         }
     }
 }
